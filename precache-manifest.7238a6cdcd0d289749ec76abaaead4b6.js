da0f02a5161d60f8505810855ee736ddca44dce5
egzonkrs <egzoonkras@hotmail.com>
2020-10-05 01:47:12 +0200

    Updates

---
+++ precache-manifest.7238a6cdcd0d289749ec76abaaead4b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb03e0f90d34f9bbd0e251717f573133",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fd1f911a80c291f23e74",
+    "url": "/monsters-rolodex/static/css/main.b487a3a6.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/monsters-rolodex/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd1f911a80c291f23e74",
+    "url": "/monsters-rolodex/static/js/main.963c3bd8.chunk.js"
+  },
+  {
+    "revision": "5d9871b43329dda10fc8",
+    "url": "/monsters-rolodex/static/js/runtime-main.98ead556.js"
+  }
+]);
